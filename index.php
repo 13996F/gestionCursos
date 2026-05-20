b0cb58699dba1545f7717ab3776652c1d2863bc4
Jorge Franco Mamani <101369250+13996F@users.noreply.github.com>
2026-05-20 09:43:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido</title>
+    <link rel="icon" type="image/png" href="image/iconogratis.png">
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
